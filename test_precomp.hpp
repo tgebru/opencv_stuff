309537f3f72b1defb312c041f9e13625c583c2a7
shirmung <shirmung@gmail.com>
2012-07-21 16:47:27 -0400

    adding remaining tests in calib3d, still need to get test data to be accessible somehow

--- test_precomp.hpp
+++ test_precomp.hpp
@@ -1,11 +1,12 @@
 #ifdef __GNUC__
 #  pragma GCC diagnostic ignored "-Wmissing-declarations"
+#  pragma GCC diagnostic ignored "-Wmissing-prototypes" //OSX
 #endif
 
 #ifndef __OPENCV_TEST_PRECOMP_HPP__
 #define __OPENCV_TEST_PRECOMP_HPP__
 
-#include "ts.hpp"
+#include "ts.hpp" //b/c it's not included in framework
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/imgproc/imgproc_c.h"
 #include "opencv2/calib3d/calib3d.hpp"
