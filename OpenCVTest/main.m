803c418f17285f8d2e733f327d42da97a9c848c5
Timnit Gebru <tgebru@gmail.com>
2012-07-27 11:49:01 -0400

    Added stuff to compile opencv_perf_test

--- OpenCVTest/main.m
+++ OpenCVTest/main.m
@@ -10,13 +10,18 @@
 //#include "test_precomp.hpp"
 
 #import "OpenCVTestAppDelegate.h"
+//#include "perf_precomp.hpp"
+
 
 int main(int argc, char *argv[])
 {
-//    cvtest::TS::ptr()->init("cv");
-//    ::testing::InitGoogleTest(&argc, argv);
-//    RUN_ALL_TESTS();
+  /* 
+    cvtest::TS::ptr()->init("cv");
+    ::testing::InitGoogleTest(&argc, argv);
+    RUN_ALL_TESTS();
     
+    //CV_PERF_TEST_MAIN(core)
+  */ 
     @autoreleasepool {
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([OpenCVTestAppDelegate class]));
     }
