615e8d89d127d92699a8f7e313c4bc4351e42f42
shirmung <shirmung@gmail.com>
2012-07-28 08:29:38 -0400

    writing xml results to iPhone

--- OpenCVTest/OpenCVTestViewController.mm
+++ OpenCVTest/OpenCVTestViewController.mm
@@ -15,12 +15,26 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+
+    BOOL fileExists = [[NSFileManager defaultManager] fileExistsAtPath:[self resultsFilePath]];
     
-    cvtest::TS::ptr()->init("cv");
-    int argc = 0;
-    char **argv = NULL;
-    ::testing::InitGoogleTest(&argc, argv);
-    RUN_ALL_TESTS();
+    if (fileExists) {
+        // for now just print to console
+        NSString *resultsString = [NSString stringWithContentsOfFile:[self resultsFilePath] encoding:NSUTF8StringEncoding error:nil];
+        NSLog(@"%@", resultsString);
+    } else {
+        cvtest::TS::ptr()->init("cv");
+        
+        int argc = 0;
+        char **argv = NULL;
+        ::testing::InitGoogleTest(&argc, argv);
+        
+        NSString *objCString = [NSString stringWithFormat:@"xml:%@", [self resultsFilePath]];
+        std::string cString = [objCString cStringUsingEncoding:[NSString defaultCStringEncoding]];
+        ::testing::GTEST_FLAG(output) = cString;
+        
+        RUN_ALL_TESTS();
+    }
 }
 
 - (void)viewDidUnload
@@ -33,6 +47,14 @@
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
     return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+- (NSString *)resultsFilePath
+{
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsDirectory = [paths objectAtIndex:0];
+    
+    return [documentsDirectory stringByAppendingPathComponent:@"results.xml"];
 }
 
 @end
